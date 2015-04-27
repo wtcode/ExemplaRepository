613c581c496de194e8b648bdae587bdff54075b2
wallacedev <wallaceat14@gmail.com>
2015-04-27 12:13:05 -0300

    Finalização cadastro NewsLetter Frontend

--- includes/newsletter_ajax.php
+++ includes/newsletter_ajax.php
@@ -1,3 +1,12 @@
 <?php 
-     echo "teste";   
+    $nome = $_POST["n"];
+    $email = $_POST["e"];
+
+    include "conect.php";
+    
+    $sql_insert = "INSERT into newslletter(nome, email)
+   			 values('$nome', '$email');";
+    
+    $result_insert = mysql_query($sql_insert,$conect);
+    $result = mysql_query($sql);
 ?>
--- js/ajax.js
+++ js/ajax.js
@@ -1,28 +1,33 @@
 $(document).ready(function(){
     $("#bttOkNewsletter").click(function(){
-        var xmlhttp;
-	if (window.XMLHttpRequest)
-	  {// code for IE7+, Firefox, Chrome, Opera, Safari
-	  xmlhttp=new XMLHttpRequest();
-	  }
-	else
-	  {// code for IE6, IE5
-	  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	  }
-	var nome = document.getElementById("txtNomeNewsletter").value;
-        var email = document.getElementById("txtEmailNewsletter").value;
-        var padrao = "^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$";
-        
-        if (nome === "Seu nome" || email === "Seu email")
-        {
-            alert("Dados incorretos");
-        }else if (nome === "" || email === "")
-        {
-            alert("Dados incorretos");
-        }else if (padrao.test(email))
-        {
-            alert("Dados incorretos - regex");
-            
-        }
+		var nome = document.getElementById("txtNomeNewsletter").value;
+	    var email = document.getElementById("txtEmailNewsletter").value;
+	    var padrao = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
+	    
+	    if (nome === "Seu nome" || email === "Seu email")
+	    {
+	        alert("Peeencha os dados corretamente!");
+	    }
+	    else if (nome === "" || email === "")
+	    {
+	        alert("Os campos não podem estar vazios!");
+	    }
+	    else if (!padrao.test(email))
+	    {
+	        alert("Informe um email válido!");
+	        
+	    }
+	    //Caso os dados sejam válidos chama a função AJAX
+	    else
+	    {
+	    	$.post("includes/newsletter_ajax.php",
+			    {
+			        n: nome,
+			        e: email
+			    },
+			    function(data, status){
+			        alert("Cadastro realizado com sucesso!");
+		    });
+	    }
     });
 });
