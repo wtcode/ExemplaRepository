529b720d1a30cf4004aaaeb28568f52cbfaa211b
PauloHenriqueVaz <paulohvazb@gmail.com>
2015-05-06 14:21:27 -0300

    Update menulateralareadeatuacao.php

--- includes/menulateralareadeatuacao.php
+++ includes/menulateralareadeatuacao.php
@@ -6,42 +6,17 @@
 ?>
 
 <div class="menuLateral">
-            
-        	<table width="230" border="0" cellspacing="0" cellpadding="0">
+       <table width="230" border="0" cellspacing="0" cellpadding="0">
             <?php
-           		 while ($linha = mysql_fetch_array($result)){
+           	while ($linha = mysql_fetch_array($result)){
             ?>
-              <tr>
-                <td colspan="2" class="tituloMenu"><a href="areadeatuacao.php?prd=<?php echo $linha['idprodutos'] ?>"><?php echo utf8_encode($linha['titulo']) ?></a></td>
-              </tr>
+	              <tr>
+	                <td colspan="2" class="tituloMenu">
+	                	<a href="areadeatuacao.php?prd=<?php echo $linha['idprodutos'] ?>"><?php echo utf8_encode($linha['titulo']) ?></a>
+	               	</td>
+	              </tr>
             <?php
-				}
+		}
             ?>
-              
-              <!-- 
-              <tr>
-                <td colspan="2" style="padding-left:19px; padding-top:10px;">
-                	<table width="180" border="0" cellspacing="0" cellpadding="0">
-                       <tr>
-                          <td style="padding-bottom:10px;"><img src="imagens/setaMenu.png" style="width:5px;"/></td>
-                          <td class="subCategoriaMenu">Institucional</td>                          
-                       </tr>
-                       <tr>
-                          <td style="padding-bottom:10px;"><img src="imagens/setaMenu.png" style="width:5px;"/></td>
-                          <td class="subCategoriaMenu">Missão</td>
-                       </tr>
-                       <tr>
-                          <td style="padding-bottom:10px;"><img src="imagens/setaMenu.png" style="width:5px;"/></td>
-                          <td class="subCategoriaMenu">Equipe</td>
-                       </tr>
-                       <tr>
-                          <td style="padding-bottom:10px;"><img src="imagens/setaMenu.png" style="width:5px;"/></td>
-                          <td class="subCategoriaMenu">Links</td>
-                       </tr>
-              		</table>
-                </td>
-              </tr>
-              -->
-            </table>
-
-        </div>
+        </table>
+</div>
