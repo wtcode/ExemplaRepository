59c1ab7264ca12baa508717ec75eed81437e5be7
wallacedev <wallaceat14@gmail.com>
2015-05-09 18:22:46 -0300

    Criação da Àrea de Atuação: Projetos Hidraulicos
    Para a imagem aparecer na aplicação local executar os  2 scripts abaixo
    no bd local:
    
    INSERT INTO `produtos` (`idprodutos`, `idcategoria`, `idsub_categoria`,
    `idhabilitado`, `titulo`, `descricao`, `destaque_home`, `destaque_topo`,
    `banner`, `preco`) VALUES
    (84, 11, 0, '1', 'Projetos Hidráulicos
    Prediais e Industriais', '<div style="text-align: justify;"><font
    face="Myriad Pro" size="3">Elabora&ccedil;&atilde;o e projetos
    hidr&aacute;ulicos prediais e industriais de acordo com necessidade do
    cliente. Dimensionamentode redes e sistemas, rotas e
    tubula&ccedil;&atilde;o, isom&eacute;tricos, listas e materiais,
    planilhas de quantidades e documenta&ccedil;&atilde;o em
    geral.</font></div>', '', '', NULL, '');
    
    INSERT INTO `imagem_produto` (`idimagem_produto`, `idprodutos`, `patch`)
    VALUES
    (106, 84, '8963.jpg');

--- includes/conteudoclipar.php
+++ includes/conteudoclipar.php
@@ -1,28 +1,26 @@
 <div class="divmain tituloPagina">Clientes</div>
 
 <div class="conteudo">
-
-<?php
-//include ("conect.php");					
-	/* $sql = "SELECT * FROM conteudo where idpagina = 10";
-	$result = mysql_query($sql);
-	$linha = mysql_fetch_array($result);
-	echo "<center>".$linha["descricao"]."</center>"; */
-include ("linksclientes.php");
-?>
-
+    <?php
+    //include ("conect.php");					
+            /* $sql = "SELECT * FROM conteudo where idpagina = 10";
+            $result = mysql_query($sql);
+            $linha = mysql_fetch_array($result);
+            echo "<center>".$linha["descricao"]."</center>"; */
+    include ("linksclientes.php");
+    ?>
 </div><?php //fim DIV CONTEUDO ?>
+
+<div class="clear"></div>
 
 <div class="divmain tituloPagina">Parceiros</div>
 
 <div class="conteudo">
-
-<?php
-	/* $sql = "SELECT * FROM conteudo where idpagina = 11";
-	$result = mysql_query($sql);
-	$linha = mysql_fetch_array($result);
-	echo "<center>".$linha["descricao"]."</center>"; */
-include ("linksparceiros.php");
-?>
-
+    <?php
+            /* $sql = "SELECT * FROM conteudo where idpagina = 11";
+            $result = mysql_query($sql);
+            $linha = mysql_fetch_array($result);
+            echo "<center>".$linha["descricao"]."</center>"; */
+    include ("linksparceiros.php");
+    ?>
 </div><?php //fim DIV CONTEUDO ?>
