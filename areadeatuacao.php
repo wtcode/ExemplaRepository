b2d775ec34f9642157e18a8ebdcc38073d8f2193
PauloHenriqueVaz <paulohvazb@gmail.com>
2015-12-29 00:18:48 -0200

    teste

--- areadeatuacao.php
+++ areadeatuacao.php
@@ -2,7 +2,7 @@
 <?php 
 include("includes/MasterPage.php"); 
 $MasterPage = new MasterPage();
-?>
+?> 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
